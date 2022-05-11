a4db79dc3e34bac5a3fac31a3bd700c5cc1e9df6
Michael Cassidy <mcassidy@MBPRO-X6KWQYQTC6.local>
2022-05-10 20:31:12 -0400

    keyboard entry

--- src/components/keyboard/keyboard.jsx
+++ src/components/keyboard/keyboard.jsx
@@ -3,16 +3,30 @@ import React, { useState, useEffect } from "react";
 const Keyboard = () => {
   // set state to represent initial value of masterGainNode
   const [keyEntry, setKeyEntry] = useState("");
+  const [freqSubmit, setFreqSubmit] = useState("");
 
   // initialize state for OscillatorNodes
+
+  const handleKeyboardInput = (e) => {
+    if (e.key === "Enter") {
+      setFreqSubmit(keyEntry);
+      setKeyEntry("");
+      document.getElementById("inputField").value = "";
+    } else if (e.key === "Backspace") {
+      setKeyEntry(keyEntry.slice(0, -1));
+    } else if (isFinite(e.key)) {
+      setKeyEntry(keyEntry.concat(e.key));
+    }
+  };
 
   return (
     <div className="Keyboard">
       <div>{keyEntry}</div>
       <input
         type="text"
-        id="one"
-        onKeyDown={(e) => setKeyEntry(keyEntry.concat(e.key))}
+        id="inputField"
+        // onKeyDown={(e) => setKeyEntry(keyEntry.concat(e.key))}
+        onKeyDown={handleKeyboardInput}
       />
     </div>
   );
